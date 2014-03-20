a1961ecfec1e3f9c60f201a2ff746b9e1cd6c9ba
Jasen Hall <jasen@cs.ucsb.edu>
2014-03-20 13:37:45 -0700

    Fixing some bugs in analysis.

--- analyze.py
+++ analyze.py
@@ -5,20 +5,22 @@ import re
 
 MAXREPEATS = 3
 
-execs = [ "graphcut/GraphCut", "graphcut-parallel/GraphCut", "graphcut-segment/GraphCut", "graphcut2/GraphCut", \
-  "graphcut2-parallel/GraphCut", "graphcut4/GraphCut", "graphcut4-parallel/GraphCut" ]
-seamsToCut = [ 5, 20, 30 ]
+#execs = [ "graphcut-parallel/GraphCut", "graphcut-segment/GraphCut", "graphcut/GraphCut", "graphcut2-parallel/GraphCut", \
+#  "graphcut2/GraphCut", "graphcut4-parallel/GraphCut", "graphcut4/GraphCut" ]
+execs = [ "graphcut/GraphCut" ]
+
+seamsToCut = [ 8, 16, 32 ]
+
+of = open("analysis1b.txt", "w");
 
 for e in execs:
-  print('Executable ' + e +':')
-
   # setup command line variables
-  workers = 2
+  workers = 8
   if e == "graphcut/GraphCut" or e == "graphcut2/GraphCut" or e == "graphcut4/GraphCut":
     workers = 1
-    maxWorker = 1
+    maxWorkers = 1
   else:
-    maxWorker = 8
+    maxWorkers = 8
 
   segments = 2
   if e == "graphcut-segment/GraphCut":
@@ -27,37 +29,40 @@ for e in execs:
     segments = 1
     maxSegments = 1
 
-  while workers <= maxWorker:
-
+  # loop over options and run programs
+  while workers <= maxWorkers:
+    print "- running with %d/%d workers" % ( workers, maxWorkers )
     while segments <= maxSegments: 
-
       for seams in seamsToCut:
-
         repeats = 0
 
-        cmd = e + " -f ratatouille1.mov " + "-v " + str(seams) + " -h 0 -r"
+        cmd = e + " -f ratatouille1-30.mov " + "-v " + str(seams) + " -h 0 -r"
 
         if maxSegments != 1:
           cmd += " -s " + str(segments)
-        if maxWorker != 1:
+        if maxWorkers != 1:
           cmd += " -w " + str(workers)
 
         s = -1
         while s != 0 and repeats < MAXREPEATS:
-          print "\tworkers: %d segments: %d seams: %d" % (workers, segments, seams)
+          of.write("%s\n" % (cmd))
+          of.write("workers: %d segments: %d seams: %d\n" % (workers, segments, seams))
+          of.flush()
+          print "%s" % (cmd)
           (s, o) = commands.getstatusoutput(cmd)
           repeats += 1
 
         multtime = int(re.search('Elapsed time: (.*)', o).group(1))
 
         if repeats == MAXREPEATS:
-          print "\tunable to complete operation"
+          of.write("\tunable to complete operation\n")
         else:
-          print "\ttime: %d" % multtime
+          of.write("\ttime: %d\n" % multtime)
 
+        of.flush()
       segments *= 2
+    workers = workers * 2
+    print "- workers incremented to %d" % ( workers )
 
-    workers *= 2
-
-
-print('\nAnalysis complete.')
+of.close()
+print '\nAnalysis complete.'
